10024f377f533996d48afab5653976c0cacca97a
VMedeiros <VMedeiros@users.noreply.github.com>
2025-12-02 18:01:33 +0000

    deploy: 03fcaabb1936f3022be5db632ab869919b62bc01

---
+++ ngsw-worker.js
@@ -0,0 +1,39 @@
+const CACHE_NAME = "judo-master-v1";
+const urlsToCache = [
+  "/Judo-Master/",
+  "/Judo-Master/index.html",
+  "/Judo-Master/styles.css",
+  "/Judo-Master/main.js",
+];
+
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(urlsToCache))
+  );
+});
+
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      if (response) {
+        return response;
+      }
+      return fetch(event.request);
+    })
+  );
+});
+
+self.addEventListener("activate", (event) => {
+  const cacheWhitelist = [CACHE_NAME];
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames.map((cacheName) => {
+          if (cacheWhitelist.indexOf(cacheName) === -1) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+});
